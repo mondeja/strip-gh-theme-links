399b8d225befff2a32b7d6219df04f3b6b291cd0
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-10-21 18:10:36 +0200

    Don't emit TS declarations for action

--- dist/action/action/inputs.d.ts
+++
@@ -1,5 +0,0 @@
-declare type keepType = "light" | "dark" | undefined;
-export declare function getFiles(strict?: boolean | undefined): Array<string>;
-export declare function getKeep(): keepType;
-export declare function getStrict(): boolean;
-export {};
--- dist/action/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @param content Content for which Github theme image links
- * will be stripped.
- * @param keep Theme variant links to keep in the content.
- * @returns Content with Github theme links stripped.
- */
-export default function stripGhThemeLinks(content: string, keep?: "light" | "dark"): string;
